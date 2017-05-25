4848d3297b58d38da68d08c10a5860524d8c8046
ivanCuculic <ivan.cuculic93@gmail.com>
2017-05-25 12:48:08 +0200

    commit

--- src/main/java/hr/java/vjezba/entitet/NewC.java
+++ src/main/java/hr/java/vjezba/entitet/NewC.java
@@ -4,4 +4,12 @@ package hr.java.vjezba.entitet;
  * Created by Xotra on 25.5.2017..
  */
 public class NewC {
+    int i;
+
+    NewC(int i) {
+        this.i = i;
+    }
+    NewC() {
+        this.i = 0;
+    }
 }
